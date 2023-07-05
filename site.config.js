b84028b54639a0dd73432f0aeddc68249e949c4c
hyunw9 <rkdgusdnr32@naver.com>
2023-07-06 01:50:44 +0900

    FEAT : describe

--- site.config.js
+++ site.config.js
@@ -2,9 +2,9 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hyunw9",
-    image: "./notion-avatar-1688006061411.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://github.com/hyunw9/morethan-log/blob/789bc45276200b7f8973bd40cf7c1d5883dba22c/notion-avatar-1688006061411.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "server developer",
-    bio: "어제보다 내일이 기대되는 개발자",
+    bio: "어제보다 내일이 기대되는 개발자\n 코드가 보이지 않는다면 새로고침 해주세요.",
     email: "rkdgusdnr32@naver.com",
     linkedin: "hyunw9",
     github: "hyunw9",
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: ``,
-      href: "",
+      name: `ShallWe`,
+      href: "https://github.com/ShallWeProject",
     },
   ],
   // blog setting (required)
